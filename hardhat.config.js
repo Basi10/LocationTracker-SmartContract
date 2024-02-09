fceda619132b2163f6e94da3a4703fbe7fca146b
Basi10 <basilelbirru@gmail.com>
2024-02-09 16:11:16 +0300

    added network

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,4 +8,12 @@ require("./tasks/faucet");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url: "http://localhost:7545", // Use the default Hardhat network URL
+      accounts: [
+        "0x5b2acb5861269f6a10f6889d734b484719a11e74410decb7b91f9bc3a91657da"
+      ]
+    }
+  }
 };
