a54f5c11d2a60eccca01be5afa241390de086a25
Basi10 <basilelbirru@gmail.com>
2024-02-09 20:36:52 +0300

    altered main page

--- admin-frontend/src/App.jsx
+++ admin-frontend/src/App.jsx
@@ -1,84 +1,13 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
-import Web3 from "web3";
 
 function App() {
-  const [balance, setBalance] = useState(0);
-
-  useEffect(() => {
-    initWeb3();
-  }, []);
-
-  const initWeb3 = async () => {
-    try {
-      if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-        await window.ethereum.enable();
-      } else if (window.web3) {
-        window.web3 = new Web3(window.web3.currentProvider);
-      } else {
-        console.error("No Ethereum provider detected. Please install MetaMask.");
-      }
-    } catch (error) {
-      console.error("Error initializing Web3:", error);
-    }
-  };
-
-  const getBalance = async () => {
-    try {
-      const address = '0x571B3E5FC25BDE17fB190Cc7fDF5e664823e5251';
-      const abi = [ 
-        {
-          "inputs": [
-            {
-              "internalType": "int256",
-              "name": "amt",
-              "type": "int256"
-            }
-          ],
-          "name": "deposit",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
-        },
-        {
-          "inputs": [],
-          "name": "getBalance",
-          "outputs": [
-            {
-              "internalType": "int256",
-              "name": "",
-              "type": "int256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        }
-      ];
-
-      const contract = new window.web3.eth.Contract(abi, address);
-
-      const result = await contract.methods.getBalance().call();
-      setBalance(() => result); // Use a callback function
-      console.log(result);
-    } catch (error) {
-      console.error("Error fetching balance:", error);
-    }
-  };
-
   return (
     <div>
-      
-      <NavBar />
-      <button onClick={getBalance}>Get Balance</button>
-      <p>Balance: {balance.toString()}</p>
+       <NavBar />
     </div>
+   
   );
+  
 }
-
 export default App;
