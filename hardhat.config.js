87639c25f8d084bec1b2f8db30d5e17627ee6bc1
Basi10 <basilelbirru@gmail.com>
2024-02-10 19:28:41 +0300

    updated network configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,68 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-require("./tasks/faucet");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("./tasks/block-number");
+require("./tasks/accounts");
+require("solidity-coverage");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
-  networks: {
-    localhost: {
-      url: "http://localhost:7545", // Use the default Hardhat network URL
-      accounts: [
-        "0x5b2acb5861269f6a10f6889d734b484719a11e74410decb7b91f9bc3a91657da"
-      ]
-    }
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "API_KEY";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "API_KEY";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
   }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
